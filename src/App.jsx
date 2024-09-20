ec40f317e3ab7ee1463637aec22f8f6c83466ed0
SCYTHE6 <divyanshuchauhan1283@gmail.com>
2024-09-20 21:53:23 +0530

    signup page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './Components/Login'
+import SignUp from './Components/SignUp'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Login/>
-    </>
+    <Router>
+      <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} /> {/* Sign-Up Page Route */}
+        {/* Other routes */}
+      </Routes>
+    </Router>
+    // <>
+    //   <Login/>
+    //   <SignUp/>
+    // </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+// tailwind.config.js
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}', // Point to your JSX/JS files
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
